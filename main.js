6322d42ab2df44a7541eae4be5380e63aca0cd3a
Rap1zer <125188052+Rap1zer@users.noreply.github.com>
2024-07-09 21:51:30 +0100

    Added normal map to sphere

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ scene.add(directionalLight);
 directionalLight.position.set(10, 15, 12);
 directionalLight.target.position.set(0, 0, 0); // Set target position
 
-const lightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
-scene.add(lightHelper);
+// const lightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+// scene.add(lightHelper);
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(gridHelper);
 
@@ -30,11 +30,12 @@ const cubesphereGenerator = new CubeSphereGenerator(10, 6);
 const loader = new THREE.TextureLoader();
 cubesphereGenerator.parent.children.forEach((child, index) => {
   if (child instanceof THREE.Mesh) {
-    loader.load(`./textures/surface map/${index}.png`, (texture) => {
-      const cubeFaceMat = new THREE.MeshStandardMaterial({
-        map: texture,
-      });
-      child.material = cubeFaceMat;
+    const surfaceMat = loader.load(`./textures/surface map/${index}.png`);
+    const normalMat = loader.load(`./textures/normal map/${index}.png`);
+    child.material = new THREE.MeshStandardMaterial({
+      map: surfaceMat,
+      normalMap: normalMat,
+      normalScale: new THREE.Vector2(2, 2),
     });
   }
 });
